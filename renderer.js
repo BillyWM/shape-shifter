991262b3985f9cffdf81c74543e1f0dfb6b1739f
Billy Wenge-Murphy <iamthebilly@gmail.com>
2020-07-09 22:18:50 -0700

    Init render loop for any <shape-shifter> elements on page

--- renderer.js
+++ renderer.js
@@ -2,10 +2,6 @@ let Two = require("two.js");
 let parser = require("./shape-shifter-parser.js");
 import "/index.scss";
 
-// let AST = parser.parse("grid of circles");
-
-// -----------------
-
 // let two = new Two({
 //     width: 600,
 //     height: 600
@@ -20,9 +16,57 @@ import "/index.scss";
 
 // -------------------
 
-let shifterTags = document.getElementsByTagName("shape-shifter");
+class Renderer {
+    constructor() {
+        this.tagList = new Map();
 
-for (let el of shifterTags) {
-    let AST = parser.parse(el.innerText);
-    console.log(AST);
+        this.init = this.init.bind(this);
+    }
+
+    init() {
+        console.log("init...");
+        this.findTags();
+        this.renderTags();
+    }
+
+    findTags() {
+        for (let tag of document.getElementsByTagName("shape-shifter")) {
+            this.tagList.set(tag, {});
+        }
+
+        console.log("found ", this.tagList.size);
+    }
+
+    parse(text) {
+        return parser.parse(text);
+    }
+
+    renderTags() {
+
+        // Update a tag to associate a bound renderer with it
+        for (let [tag, obj] of this.tagList.entries()) {
+            // let obj = this.tagList.get(tag);
+            obj.renderStep = this.renderStep.bind(this, tag);
+            this.tagList.set(tag, obj);
+            this.render(tag);
+        }
+    }
+
+    // Render a single element
+    render(el) {
+        let script = el.innerText;
+        let AST = this.parse(script);
+        console.log("AST", AST);
+
+        window.requestAnimationFrame(this.tagList.get(el).renderStep);
+    }
+
+    // TODO: saving bound renderers
+    //      hashmap; key: element ref, values bound render function, state information, parsed AST
+    renderStep(el, timestamp) {
+        window.requestAnimationFrame(this.tagList.get(el).renderStep);
+    }
 }
+
+let renderer = new Renderer();
+window.addEventListener('DOMContentLoaded', renderer.init);
